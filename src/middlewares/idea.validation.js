6b9aa9b725bb1e6337d9f27a27def8c51ae365ac
Nada Shaban <nadashaban971@gmail.com>
2025-09-12 00:59:08 +0300

    Add validateIdeaForUpdate and edit validateIdea

--- src/middlewares/idea.validation.js
+++ src/middlewares/idea.validation.js
@@ -1,17 +1,57 @@
 import {body, validationResult} from 'express-validator';
 
 export function validateIdea(){
-    return[
-        body('content').notEmpty().withMessage('content can not be empty').isLength({min : 3}).withMessage('Content must be at least 3 characters'),
-        body('reminderMeAt').isISO8601().withMessage('reminder date must be valid'),
-        body('priority').optional().isIn(['low', 'mid' ,'high']).withMessage("Priority must be low, medium, or high"),
+    return [
+        body("content")
+            .notEmpty()
+            .withMessage("Content cannot be empty")
+            .isLength({ min: 3 })
+            .withMessage("Content must be at least 3 chars"),
 
-        (req,res,next)=>{
-            const err = validationResult(req);
-            if(!err.isEmpty()){
-                return res.status(400).json({errors : err.array()});
+        body("reminderMeAt")
+            .optional()
+            .isISO8601()
+            .withMessage("Please enter a valid date (e.g., 2025-09-20)")
+            .toDate(),
+
+        body("priority")
+            .optional()
+            .isIn(["low", "medium", "high"])
+            .withMessage("Priority must be low, medium, or high"),
+
+        (req, res, next) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
             }
             next();
-        }
-    ]
+        },
+    ];
+}
+
+
+export function validateIdeaForUpdate() {
+    return [
+        body("content")
+            .optional()
+            .isLength({ min: 3 }).withMessage("Content must be at least 3 characters"),
+
+        body("reminderMeAt")
+            .optional()
+            .isISO8601()
+            .withMessage("reminder date must be valid")
+            .toDate(),
+
+        body("priority")
+            .optional()
+            .isIn(["low", "medium", "high"]).withMessage("Priority must be low, mid, or high"),
+
+        (req, res, next) => {
+            const err = validationResult(req);
+            if (!err.isEmpty()) {
+                return res.status(400).json({ errors: err.array() });
+            }
+            next();
+        },
+    ];
 }
