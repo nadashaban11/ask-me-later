aafce44a84ae74ccd27210d90c3ec5d53ddcbea7
Nada Shaban <nadashaban971@gmail.com>
2025-09-14 10:50:59 +0300

    Add hashing and install req dep

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import { type } from "os";
+import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
     name : {
@@ -22,6 +22,11 @@ const userSchema = new mongoose.Schema({
     }
 
 );
+
+userSchema.pre("save", async (next)=>{
+    this.password = await bcrypt.hash(this.password,10);
+    next();
+})
 
 
 const User = mongoose.model('User' , userSchema);
