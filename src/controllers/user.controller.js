5f6306937648798e6048469b4ec9b57d8342bf53
Nada Shaban <nadashaban971@gmail.com>
2025-09-15 01:34:48 +0300

    fear(user): add sign in and validate it

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import bcrypt from 'bcrypt'
 
 import User from '../models/user.model.js';
 
@@ -26,4 +27,31 @@ export async function signUp(req,res) {
         res.status(500).json({ success: false, message: "Error registering user", error: err.message });
     }
 
+};
+
+export async function signIn(req, res){
+    const { email, password } = req.body;
+    try{
+        const user = await User.findOne({ email }).select("+password");
+        if (!user) {
+            return res.status(400).json({ success: false, message: "Invalid email or password" });
+        }
+
+        const samePass = bcrypt.compare(password, user.password);
+        if(!samePass){
+            return res.status(400).json({ success: false, message: "Invalid email or password" });
+        }
+
+        const userRes = user.toObject();
+        delete userRes.password;
+
+        res.status(200).json({
+            success: true,
+            message: "User logged in successfully",
+            user: userRes
+        });
+    }
+    catch(error){
+        res.status(500).json({ success: false, message: "Server error", error: err.message });
+    }
 };
